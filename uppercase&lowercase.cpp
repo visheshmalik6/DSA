0a0c2731999baccab6dbc7c1d6b5c3a2b34ad1f8
Vishesh Malik <154320302+visheshmalik6@users.noreply.github.com>
2026-08-22 17:52:34 +0530

    Add character case checker in uppercase&lowercase.cpp

---
+++ uppercase&lowercase.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  char n;
+  cout<<"Enter your character";
+  cin >> n;
+  if(n>='a' && n<='z'){
+    cout<<"Your character is lower case";}
+  else{
+    cout<<"your character is upper case";}
+  
+}
