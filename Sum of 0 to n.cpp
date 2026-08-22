8980cae01e0267723ba0375dce6e6603b580ed6a
Vishesh Malik <154320302+visheshmalik6@users.noreply.github.com>
2026-08-22 18:26:09 +0530

    Implement sum of integers from 0 to n
    This program calculates the sum of integers from 0 to n and prints the cumulative sum at each step.

---
+++ Sum of 0 to n.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    int j = 0;
+    cout<<"enter your num\n";
+    cin>>n;
+    for(int i=0 ; i<=n ;i++){
+        j += i;
+        cout<<j<<endl;
+    }
+  
+}
