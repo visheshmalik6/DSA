e311c42f3b0d120072b43f084725372cbd943959
Vishesh Malik <154320302+visheshmalik6@users.noreply.github.com>
2026-08-22 18:34:31 +0530

    Implement sum of odd numbers from 0 to n
    Added functionality to sum and print odd numbers from 0 to n.

--- Sum of 0 to n.cpp
+++ Sum of 0 to n.cpp
@@ -12,3 +12,21 @@ int main() {
     }
   
 }
+
+// sum of only odds number from 0 to n
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    int j = 0;
+    cout<<"enter your num\n";
+    cin>>n;
+    for(int i=0 ; i<=n;i++){
+        if(i%2!=0){
+            j += i;
+            cout<<j<<endl;
+        }
+    }
+  
+}
