33af9e3d397167d5bf269ac301d20ae0d670fb14
Vishesh Malik <154320302+visheshmalik6@users.noreply.github.com>
2026-08-24 18:52:07 +0530

    Add inverted pyramid pattern printing in Patterns.cpp

--- Patterns.cpp
+++ Patterns.cpp
@@ -57,3 +57,28 @@ int main() {
 	}
 
 }
+// inverted piramid
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	int num =1;
+	cout<<"enter number\n";
+	cin>>n;
+	for(int i =0 ; i<n; i++){
+	    cout<<"\n";
+	    
+	    
+	    for(int j=0 ; j<i+1 ; j++){
+	        
+	        cout<<" ";
+	        
+	        
+	    }
+	    for(int j=0 ; j<n-i ; j++){
+	            cout<<i+1<<" ";
+	        }
+	}
+
+}
