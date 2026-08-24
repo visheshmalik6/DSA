2f4ae0c9dc75a06827d154525d2c2c8bb5e5b987
Vishesh Malik <154320302+visheshmalik6@users.noreply.github.com>
2026-08-24 12:54:39 +0530

    Add Floyd's triangle pattern implementation
    Implement Floyd's triangle pattern in C++.

--- Patterns.cpp
+++ Patterns.cpp
@@ -35,3 +35,25 @@ int main() {
 	}
 
 }
+// floyds triangle pattern 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	int num =1;
+	cout<<"enter number\n";
+	cin>>n;
+	for(int i =0 ; i<n; i++){
+	    cout<<"\n";
+	    
+	    
+	    for(int j=0 ; j<i+1 ; j++){
+	        
+	        cout<<num<<" ";
+	        num++;
+	        
+	    }
+	}
+
+}
